a392f9e6b84c616f84df5448b31fc29bdfedbd01
Bryn Hastings <brynhastings@google.com>
2023-03-17 14:12:16 +0000

    Returns blank object for error handling
    Change-Id: I3fe5e0322aa226460675fad7669f7ec0e3726678

--- core_functionality/spreadsheet_reader.js
+++ core_functionality/spreadsheet_reader.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2022 Google LLC
+ * Copyright 2023 Google LLC
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -63,7 +63,7 @@ function getConditions() {
   let weight =
       sheet.getRange(firstConditionRow, weightingColumn).getDisplayValue();
   if (weight == '') {
-    return
+    return []
   };
   let conditions = [];
   while (weight != '') {
