40eb4f7a55aff2549c744f8734c3bd9cfa1b7a46
Bryn Hastings <brynhastings@google.com>
2023-03-17 13:13:30 +0000

    Adds tracking to secondary code base
    Change-Id: Ie40d823aec3d1851cf52b2796fe5858db9cde056

---
+++ extra_functionality/tracking.js
@@ -0,0 +1,41 @@
+/**
+ * Copyright 2023 Google LLC
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/**
+ * Writes data to the tracking sheet based on what function was run and whether
+ * or not it triggered an error
+ * @param {string} functionName The name of the function being executed
+ * @param {string=} error The error message
+ */
+
+function logFunctionRun(functionName, error) {
+  let ss = SpreadsheetApp.getActiveSpreadsheet();
+  let sheet = ss.getSheetByName('Error Tracking');
+  let ss_id = SpreadsheetApp.getActiveSpreadsheet().getId();
+  let adv_id = ADVERTISER_ID;
+  let user = Session.getActiveUser().getEmail();
+  let status = '';
+  if (error == undefined) {
+    error = '';
+    status = 'SUCCESS';
+  } else {
+    error = error;
+    status = 'ERROR';
+  };
+  ss.setSpreadsheetTimeZone(Session.getScriptTimeZone());
+  let ts = new Date();
+  sheet.appendRow([ts, ss_id, adv_id, user, functionName, status, error]);
+}
